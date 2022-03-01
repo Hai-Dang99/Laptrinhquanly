94355e4b5b08d2d4261614006b70720038e029c0
Hai-Dang99 <haidang250899@gmail.com>
2022-03-01 08:51:00 +0700

    1/3

---
+++ LHDBaiThucHanh214/LHDBaiThucHanh214/Controllers/DemoController.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace LHDBaiThucHanh214.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Demo(string Masinhvien, string HoTenSinhVien)
+        {
+            ViewBag.Thongtin = Masinhvien + " " + HoTenSinhVien;
+            return View();
+        }
+    }
+}
---
+++ ThucHanh/ThucHanh/Controllers/DemoController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ThucHanh.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        public ActionResult Demo()
+        {
+            ViewBag.Message = "";
+            return View();
+        }
+        [HttpPost]
+        public ActionResult Index(string Masinhvien, string HoTenSinhVien)
+        {
+            ViewBag.Thongtin = Masinhvien + " " + HoTenSinhVien;
+            return View();
+        }
+    }
+}
